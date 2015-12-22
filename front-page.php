2328f32f52be89ee51cf0a52e622fbd890210649
Jason Britton <velograph@gmail.com>
2015-12-22 12:09:41 -0800

    Homepage

--- front-page.php
+++ front-page.php
@@ -12,9 +12,35 @@
 
 get_header(); ?>
 
-	<div class="content-area">
+<script>
+	jQuery(document).ready(function(){
 
-		<section class="section">
+		jQuery('.exploration-slider').slick({
+			arrows: false,
+			dots: false,
+			autoplay: false,
+			autoplaySpeed: 3000,
+			pauseOnHover: true,
+			centered: true,
+			mobileFirst: true,
+		    lazyLoad: 'ondemand',
+		});
+
+		jQuery('.outfitting-slider').slick({
+			arrows: true,
+			dots: false,
+			slidesToShow: 5,
+			autoplay: false,
+			centered: true,
+			mobileFirst: true,
+		    lazyLoad: 'ondemand',
+		});
+
+	});
+</script>
+	<div class="front-page content-area">
+
+		<section class="exploration section">
 
 			<div class="section-portal">
 
@@ -28,13 +54,48 @@ get_header(); ?>
 
 				    if($query->have_posts()) : ?>
 
-				    <?php while($query->have_posts()) : ?>
+					<div class="exploration-slider">
 
-				        <?php $query->the_post(); ?>
+					    <?php while($query->have_posts()) : ?>
 
-				        <h1><?php the_title() ?></h1>
+					        <?php $query->the_post(); ?>
 
-				    <?php endwhile; ?>
+							<div class="slide">
+
+								<?php $mobile_page_banner = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'product-banner-mobile'); ?>
+								<?php $tablet_page_banner = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'product-banner-tablet'); ?>
+								<?php $desktop_page_banner = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'product-banner-desktop'); ?>
+								<?php $retina_page_banner = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'product-banner-retina'); ?>
+
+								<picture class="picture document-header-image">
+									<!--[if IE 9]><video style="display: none"><![endif]-->
+									<source
+										srcset="<?php echo $mobile_page_banner[0]; ?>"
+										media="(max-width: 500px)" />
+									<source
+										srcset="<?php echo $tablet_page_banner[0]; ?>"
+										media="(max-width: 860px)" />
+									<source
+										srcset="<?php echo $desktop_page_banner[0]; ?>"
+										media="(max-width: 1180px)" />
+									<source
+										srcset="<?php echo $retina_page_banner[0]; ?>"
+										media="(min-width: 1181px)" />
+									<!--[if IE 9]></video><![endif]-->
+									<img srcset="<?php echo $image[0]; ?>">
+								</picture>
+
+								<div class="slide-caption">
+									<?php $post_type = get_post_type_object( get_post_type($post) ); ?>
+									<span class="post-type"><?php echo $post_type->labels->singular_name; ?>:&nbsp;</span>
+									<span class="post-title"><?php the_title() ?>&nbsp;&gt;</span>
+								</div>
+
+							</div>
+
+					    <?php endwhile; ?>
+
+					</div>
 
 				<?php endif; ?>
 
@@ -42,23 +103,25 @@ get_header(); ?>
 
 			<div class="section-supporting">
 
-				icon
+				<div>
+					<img class="section-icon" src="<?php the_field('section_icon', 2060); ?>" alt="exploration" />
+				</div>
 
 				<h1>1. Exploration</h1>
 
 				<?php the_field('section_description', 2060); ?>
-				<?php the_field('homepage_link_text', 2060); ?>
+				<a href="/exploration"><?php the_field('homepage_link_text', 2060); ?></a>
 
 			</div>
 
 		</section>
 
-		<section class="section">
+		<section class="outfitting section">
 
 			<?php the_field('section_description', 2100); ?>
 			<?php the_field('homepage_link_text', 2100); ?>
 
-			icon
+			<img class="section-icon" src="<?php the_field('section_icon', 2100); ?>" alt="exploration" />
 
 			<h1>2. Outfitting</h1>
 
@@ -67,28 +130,46 @@ get_header(); ?>
 				$args = array(
 					'post_type' => array('product'),
 					'posts_per_page' => 5,
+					'tax_query' => array(
+						array(
+							'taxonomy' => 'product_cat',
+							'field' => 'slug',
+							'terms' => 'guided-expedition',
+							'operator' => 'NOT IN',
+						),
+					),
 				);
 				$query = new WP_Query($args);
 
 				if($query->have_posts()) : ?>
 
-				<?php while($query->have_posts()) : ?>
+				<div class="outfitting-slider">
 
-					<?php $query->the_post(); ?>
+					<?php while($query->have_posts()) : ?>
 
-					<h1><?php the_title() ?></h1>
+						<?php $query->the_post(); ?>
 
-				<?php endwhile; ?>
+						<div class="product-portal slide">
+							<div class="product-thumbnail">
+								<?php the_post_thumbnail('portal-tablet'); ?>
+							</div>
+							<h5><?php the_title() ?></h5>
+							<h6>product short description</h6>
+						</div>
+
+					<?php endwhile; ?>
+
+				</div>
 
 			<?php endif; ?>
 
 		</section>
 
-		<section class="section">
+		<section class="expeditions section">
 
 			<div class="section-supporting">
 
-				icon
+				<img class="section-icon" src="<?php the_field('section_icon', 2102); ?>" alt="exploration" />
 
 				<h1>3. Expeditions</h1>
 
@@ -120,7 +201,22 @@ get_header(); ?>
 
 						<?php $query->the_post(); ?>
 
-						<h1><?php the_title() ?></h1>
+						<?php $mobile = wp_get_attachment_image_src(get_field('tall_trip_banner'), 'mobile'); ?>
+						<?php $retina = wp_get_attachment_image_src(get_field('wide_trip_banner'), 'retina'); ?>
+
+						<a href="<?php the_permalink(); ?>">
+							<picture>
+								<!--[if IE 9]><video style="display: none"><![endif]-->
+								<source
+									srcset="<?php echo $mobile[0]; ?>"
+									media="(max-width: 768px)" />
+								<source
+									srcset="<?php echo $retina[0]; ?>"
+									media="(min-width: 769px)" />
+								<!--[if IE 9]></video><![endif]-->
+								<img srcset="<?php echo $image[0]; ?>">
+							</picture>
+						</a>
 
 					<?php endwhile; ?>
 
@@ -130,40 +226,68 @@ get_header(); ?>
 
 		</section>
 
-		<section class="section">
+		<section class="partnerships section">
 
 			<div class="section-portal">
 
-				<?php
+				<?php $mobile = wp_get_attachment_image_src( get_post_thumbnail_id( 2104 ), 'mobile' ); ?>
+				<?php $tablet = wp_get_attachment_image_src( get_post_thumbnail_id( 2104 ), 'tablet' ); ?>
+				<?php $desktop = wp_get_attachment_image_src( get_post_thumbnail_id( 2104 ), 'desktop' ); ?>
+				<?php $retina = wp_get_attachment_image_src( get_post_thumbnail_id( 2104 ), 'retina' ); ?>
 
-					$args = array(
-						'post_type' => array('partnership'),
-						'posts_per_page' => 4,
-					);
-					$query = new WP_Query($args);
-
-					if($query->have_posts()) : ?>
-
-					<?php while($query->have_posts()) : ?>
-
-						<?php $query->the_post(); ?>
-
-						<h1><?php the_title() ?></h1>
-
-					<?php endwhile; ?>
-
-				<?php endif; ?>
+				<picture>
+					<!--[if IE 9]><video style="display: none;"><![endif]-->
+					<source
+						srcset="<?php echo $mobile[0]; ?>"
+						media="(max-width: 500px)" />
+					<source
+						srcset="<?php echo $tablet[0]; ?>"
+						media="(max-width: 860px)" />
+					<source
+						srcset="<?php echo $desktop[0]; ?>"
+						media="(max-width: 1180px)" />
+					<source
+						srcset="<?php echo $retina[0]; ?>"
+						media="(min-width: 1181px)" />
+					<!--[if IE 9]></video><![endif]-->
+					<img srcset="<?php echo $image[0]; ?>">
+				</picture>
 
 			</div>
 
 			<div class="section-supporting">
 
-				icon
+				<img class="section-icon" src="<?php the_field('section_icon', 2104); ?>" alt="exploration" />
 
 				<h1>4. Partnerships</h1>
 
 				<?php the_field('section_description', 2104); ?>
 				<?php the_field('homepage_link_text', 2104); ?>
+
+				<?php
+
+				    $args = array(
+				        'post_type' => 'partnership',
+						'posts_per_page' => 4,
+				    );
+				    $query = new WP_Query($args);
+
+				    if($query->have_posts()) : ?>
+
+					<div class="partner-logos">
+
+					    <?php while($query->have_posts()) : ?>
+
+					        <?php $query->the_post(); ?>
+
+			        		<span><img src="<?php the_field('company_logo'); ?>" alt="partnership_logo" /></span>
+
+				    	<?php endwhile; ?>
+
+					</div>
+
+				<?php endif; ?>
+
 
 			</div>
 
--- functions.php
+++ functions.php
@@ -148,6 +148,13 @@ function wcs_woo_remove_reviews_tab($tabs) {
  return $tabs;
 }
 
+// Allow SVG upload
+function cc_mime_types($mimes) {
+  $mimes['svg'] = 'image/svg+xml';
+  return $mimes;
+}
+add_filter('upload_mimes', 'cc_mime_types');
+
 // Limberlost Logo on login
 function custom_login_logo() {
 	echo '<style type="text/css">
