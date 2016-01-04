1a11d7641649a6f45f3fb225d409202b74cd1e85
Jason Britton <velograph@gmail.com>
2016-01-04 11:20:19 -0800

    Simple gear treatment for explorations

--- single.php
+++ single.php
@@ -181,7 +181,7 @@ get_header(); ?>
 						</div>
 					</div>
 
-					<div class="exploration-section route-content">
+					<div class="exploration-section exploration-content">
 
 						<?php the_content(); ?>
 
@@ -331,7 +331,7 @@ get_header(); ?>
 
 				</div>
 
-				<div class="exploration-section route-content">
+				<div class="exploration-section exploration-content">
 
 					<?php the_content(); ?>
 
@@ -369,10 +369,124 @@ get_header(); ?>
 
 			<?php if( has_term('gear', 'category') ) : ?>
 			<!-- Gear! -->
-			<div class="exploration-term">
-				Gear Review
+			<div class="exploration-banner">
+
+				<?php $mobile = wp_get_attachment_image_src(get_field('route_banner'), 'banner-mobile'); ?>
+				<?php $desktop = wp_get_attachment_image_src(get_field('route_banner'), 'banner-desktop'); ?>
+				<?php $retina = wp_get_attachment_image_src(get_field('route_banner'), 'banner-retina'); ?>
+
+				<picture>
+					<!--[if IE 9]><video style="display: none"><![endif]-->
+					<source
+						srcset="<?php echo $mobile[0]; ?>"
+						media="(max-width: 500px)" />
+					<source
+						srcset="<?php echo $desktop[0]; ?>"
+						media="(max-width: 1180px)" />
+					<source
+						srcset="<?php echo $retina[0]; ?>"
+						media="(min-width: 1181px)" />
+					<!--[if IE 9]></video><![endif]-->
+					<img srcset="<?php echo $image[0]; ?>">
+				</picture>
+
 			</div>
-			<h1><?php the_title(); ?></h1>
+
+			<div class="content-area">
+
+				<div class="exploration-term">
+					Gear Review
+				</div>
+				<h1 class="exploration-title"><?php the_title(); ?></h1>
+
+				<div class="exploration-section gallery-container">
+
+					<div class="main-image">
+
+						<?php if( get_field('gallery') ) : ?>
+
+							<?php
+
+							$images = get_field('gallery');
+
+							if( $images ): ?>
+								<?php foreach( $images as $image ): ?>
+
+									<picture class="portal-image">
+										<!--[if IE 9]><video style="display: none"><![endif]-->
+										<source
+											srcset="<?php echo $image['sizes']['portal-mobile']; ?>"
+											media="(max-width: 500px)" />
+										<source
+											srcset="<?php echo $image['sizes']['portal-tablet']; ?>"
+											media="(max-width: 860px)" />
+										<source
+											srcset="<?php echo $image['sizes']['portal-desktop']; ?>"
+											media="(max-width: 1180px)" />
+										<source
+											srcset="<?php echo $image['sizes']['portal-retina']; ?>"
+											media="(min-width: 1181px)" />
+										<!--[if IE 9]></video><![endif]-->
+										<img srcset="<?php echo $image['sizes']['portal-desktop']; ?>">
+									</picture>
+
+								<?php endforeach; ?>
+							<?php endif; ?>
+
+						<?php else: ?>
+
+							<?php $mobile_page_banner = wp_get_attachment_image_src(get_post_thumbnail_id( $post->ID ), 'portal-mobile'); ?>
+							<?php $tablet_page_banner = wp_get_attachment_image_src(get_post_thumbnail_id( $post->ID ), 'portal-tablet'); ?>
+							<?php $desktop_page_banner = wp_get_attachment_image_src(get_post_thumbnail_id( $post->ID ), 'portal-desktop'); ?>
+							<?php $retina_page_banner = wp_get_attachment_image_src(get_post_thumbnail_id( $post->ID ), 'portal-retina'); ?>
+
+							<picture class="single-image">
+								<!--[if IE 9]><video style="display: none"><![endif]-->
+								<source
+									srcset="<?php echo $mobile_page_banner[0]; ?>"
+									media="(max-width: 500px)" />
+								<source
+									srcset="<?php echo $tablet_page_banner[0]; ?>"
+									media="(max-width: 860px)" />
+								<source
+									srcset="<?php echo $desktop_page_banner[0]; ?>"
+									media="(max-width: 1180px)" />
+								<source
+									srcset="<?php echo $retina_page_banner[0]; ?>"
+									media="(min-width: 1181px)" />
+								<!--[if IE 9]></video><![endif]-->
+								<img srcset="<?php echo $image[0]; ?>">
+							</picture>
+
+						<?php endif; ?>
+
+					</div>
+
+					<div class="image-thumbs">
+
+						<?php
+
+						$images = get_field('gallery');
+
+						if( $images ): ?>
+							<?php foreach( $images as $image ): ?>
+
+								<img src="<?php echo $image['sizes']['thumbnail']; ?>" alt="<?php echo $image['alt']; ?>" />
+
+							<?php endforeach; ?>
+						<?php endif; ?>
+
+					</div>
+
+				</div>
+
+				<div class="exploration-section exploration-content">
+
+					<?php the_content(); ?>
+
+				</div>
+
+			</div>
 			<?php endif; ?>
 
 		<?php endwhile; // end of the loop. ?>
